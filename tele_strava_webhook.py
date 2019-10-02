5afa151d53a637cbbb19816da50044b0ed710653
Kenny Chong <kenny.chong@u.nus.edu>
2019-10-02 20:02:50 +0800

    Update tele_strava_webhook.py

--- tele_strava_webhook.py
+++ tele_strava_webhook.py
@@ -11,13 +11,19 @@ app = Flask(__name__)
 
 
 
-@app.route("/stravaresponse", methods = ['GET','POST'])
+@app.route("/stravaresponse", methods = ['GET'])
 def strava_response():
     if request.args.get('hub.mode') == 'subscribe':
         payload = {'hub.challenge': request.args['hub.challenge']}
         jsonpayload = json.dumps(payload)
     return json.dumps(payload)
 
+"""
+
 
 r = requests.post(url = strava_webhook_url, data = {'client_id': client_id, "client_secret": client_secret, 'callback_url': f'{weblink}/stravaresponse',
                                                     'verify_token': 'STRAVA'})
+"""
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0')
